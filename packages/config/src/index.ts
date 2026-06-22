ebd4213991fbd231359544267cc98c067bf818ac
arin-gupta06 <24cd10ar15@mitsgwl.ac.in>
2026-06-23 00:20:41 +0530

    Added more restrictions to por numbers and adding context to logger factory

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -8,10 +8,15 @@ dotenv.config({
 
 const envSchema = z.object(
     {
-        PORT: z.string().default("3000"),
-        DATABASE_URL: z.string(),
+        NODE_ENV: z.enum([
+           "development",
+           "test",
+           "production",
+        ]).default("development"),
+        PORT: z.coerce.number().int().min(1).max(65535).default(3000),
+        DATABASE_URL: z.url(),
         REDIS_HOST: z.string().default("localhost"),
-        REDIS_PORT: z.string().default("6379"),
+        REDIS_PORT: z.coerce.number().int().min(1).max(65535).default(6379),
 
     }
 );
@@ -19,6 +24,7 @@ const envSchema = z.object(
 const env = envSchema.parse(process.env);
 
 export const config = {
+    environment: env.NODE_ENV,
     port: Number(env.PORT),
     databaseUrl: env.DATABASE_URL,
 
--- packages/logger/src/logger-factory.ts
+++ packages/logger/src/logger-factory.ts
@@ -1,7 +1,8 @@
-import { logger } from "./index";
+import { LOG_CONTEXT, logger } from "./index";
 
 export const createLogger = (
-    context: string,
+    context: 
+    (typeof LOG_CONTEXT)[keyof typeof LOG_CONTEXT],
 ) => {
     return logger.child({
         context,
