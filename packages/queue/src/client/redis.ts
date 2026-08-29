81ea3d808a2caef246da8286c2889beb99f63b7f
arin-gupta06 <24cd10ar15@mitsgwl.ac.in>
2026-08-29 12:46:32 +0530

    feat: implement redis client connection with custom retry strategy

--- packages/queue/src/client/redis.ts
+++ packages/queue/src/client/redis.ts
@@ -1,11 +1,18 @@
 import IORedis from "ioredis";
-export const redisConnection = new IORedis({
-    host: process.env.REDIS_HOST || "localhost",
-    port: Number(process.env.REDIS_PORT) || 6379,
 
-    maxRetriesPerRequest: null,
-
-    retryStrategy(time) {
-        return Math.min(time * 50, 2000);
-    },
-});
+const redisUrl = process.env.REDIS_URL;
+export const redisConnection = redisUrl
+    ? new IORedis(redisUrl, {
+          maxRetriesPerRequest: null,
+          retryStrategy(time) {
+              return Math.min(time * 50, 2000);
+          },
+      })
+    : new IORedis({
+          host: process.env.REDIS_HOST || "localhost",
+          port: Number(process.env.REDIS_PORT) || 6379,
+          maxRetriesPerRequest: null,
+          retryStrategy(time) {
+              return Math.min(time * 50, 2000);
+          },
+      });
